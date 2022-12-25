83374b26deae6dcdd36e6bacf578179a118bb7f3
chenk <hen.keinan@gmail.com>
2022-12-25 12:23:38 +0200

    chore: add build (#10)
    * chore: add build
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: add build
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM alpine:3.16.2
-
-COPY node-collector /usr/local/bin/node-collector
-
-ENTRYPOINT ["node-collector"]
---
+++ cmd/node-collector/main.go
@@ -0,0 +1,7 @@
+package main
+
+import command "github.com/aquasecurity/k8s-node-collector/pkg/command"
+
+func main() {
+	command.Execute()
+}
